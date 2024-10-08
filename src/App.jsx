a6da8d0452b65705edb032e03dc404b27edfcd58
miguel-willians <miiguel.wso@gmail.com>
2024-10-08 16:21:27 -0300

    Adicionando a Context API à Cities Context

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { CitiesProvider } from "./contexts/CitiesContext";
 
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
@@ -13,50 +13,26 @@ import City from "./components/City";
 import Form from "./components/Form";
 
 function App() {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  const BASE_URL = "http://localhost:9000";
-
-  useEffect(function () {
-    async function fetchCities() {
-      try {
-        setIsLoading(true);
-        const res = await fetch(`${BASE_URL}/cities`);
-        const data = await res.json();
-        setCities(data);
-      } catch {
-        alert("There was an error while fetching data...");
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    fetchCities();
-  }, []);
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Homepage />} />
-        <Route path="product" element={<Product />} />
-        <Route path="pricing" element={<Pricing />} />
-        <Route path="login" element={<Login />} />
-        <Route path="app" element={<AppLayout />}>
-          <Route index element={<Navigate to="cities" />} />{" "}
-          {/* Navigate serve como um redirecionamento de forma declarativa, ao contrário do useNavigate(). Nesse caso o usuário sempre irá cair no index que, por sua vez, o redicionará para cities. O replace serve para colocar o elemento atual na pilha de histórico */}
-          <Route
-            path="cities"
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
-          <Route path="cities/:id" element={<City />} />
-          <Route
-            path="countries"
-            element={<CountryList cities={cities} isLoading={isLoading} />}
-          />
-          <Route path="form" element={<Form />} />
-        </Route>
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <CitiesProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Homepage />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="login" element={<Login />} />
+          <Route path="app" element={<AppLayout />}>
+            <Route index element={<Navigate to="cities" />} />{" "}
+            {/* Navigate serve como um redirecionamento de forma declarativa, ao contrário do useNavigate(). Nesse caso o usuário sempre irá cair no index que, por sua vez, o redicionará para cities. O replace serve para colocar o elemento atual na pilha de histórico */}
+            <Route path="cities" element={<CityList />} />
+            <Route path="cities/:id" element={<City />} />
+            <Route path="countries" element={<CountryList />} />
+            <Route path="form" element={<Form />} />
+          </Route>
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </CitiesProvider>
   );
 }
 
