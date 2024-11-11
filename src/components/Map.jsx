cef2129cbd388408bbc6b1a8196c5029cbb44611
miguel-willians <miiguel.wso@gmail.com>
2024-11-11 17:55:57 -0300

    Melhorando o cities provider

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -57,6 +57,7 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        {console.log(cities)}
         {cities.map((city) => (
           <Marker
             position={[city.position.lat, city.position.lng]}
