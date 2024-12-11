3644a65304cf81f74132ee42fb83e5c2a0f87406
miguel-willians <miiguel.wso@gmail.com>
2024-12-11 15:25:59 -0300

    Implementando otimização com a técnica de lazy loading e Suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,63 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { CitiesProvider } from "./contexts/CitiesContext";
 
-import Product from "./pages/Product";
-import Pricing from "./pages/Pricing";
-import Login from "./pages/Login";
-import Homepage from "./pages/Homepage";
-import PageNotFound from "./pages/PageNotFound";
-import AppLayout from "./pages/AppLayout";
+import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/FakeAuthContext";
+import ProtectedRoute from "./pages/ProtectedRoute";
+
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
-import { AuthProvider } from "./contexts/FakeAuthContext";
-import ProtectedRoute from "./pages/ProtectedRoute";
+import SpinnerFullPage from "./components/SpinnerFullPage";
+
+// import Product from "./pages/Product";
+// import Pricing from "./pages/Pricing";
+// import Login from "./pages/Login";
+// import Homepage from "./pages/Homepage";
+// import AppLayout from "./pages/AppLayout";
+// import PageNotFound from "./pages/PageNotFound";
+
+// Lazy loading --> Conceito de otimizar o bundle através da técnica "lazy loading". Quando o usuário faz uma request no servidor de uma aplicação React, o servido envia um bundle (um grande arquivo js) que conterá tudo que o usuário precisa para roda a SPA na sua máquina. Essa técnica visa dividir o bundle em páginas específicas para que o usuário envie requests quando precisar acessar uma.
+
+const Homepage = lazy(() => import("./pages/Homepage"));
+const Product = lazy(() => import("./pages/Product"));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const Login = lazy(() => import("./pages/Login"));
+const AppLayout = lazy(() => import("./pages/AppLayout"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
+
+// Suspence API --> A Suspence API é um recurso do React moderno que permite que alguns componentes sejam suspensos, fazendo com que eles esperem que algo aconteça. No caso, podemos mostrar um fallback enquanto os componentes carregam.
 
 function App() {
   return (
     <AuthProvider>
       <CitiesProvider>
         <BrowserRouter>
-          <Routes>
-            <Route index element={<Homepage />} />
-            <Route path="product" element={<Product />} />
-            <Route path="pricing" element={<Pricing />} />
-            <Route path="login" element={<Login />} />
-            <Route
-              path="app"
-              element={
-                <ProtectedRoute>
-                  <AppLayout />
-                </ProtectedRoute>
-              }
-            >
-              <Route index element={<Navigate to="cities" />} />
-              {/* Navigate serve como um redirecionamento de forma declarativa, ao contrário do useNavigate(). Nesse caso o usuário sempre irá cair no index que, por sua vez, o redicionará para cities. O replace serve para colocar o elemento atual na pilha de histórico */}
-              <Route path="cities" element={<CityList />} />
-              <Route path="cities/:id" element={<City />} />
-              <Route path="countries" element={<CountryList />} />
-              <Route path="form" element={<Form />} />
-            </Route>
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
+          <Suspense fallback={<SpinnerFullPage />}>
+            <Routes>
+              <Route index element={<Homepage />} />
+              <Route path="product" element={<Product />} />
+              <Route path="pricing" element={<Pricing />} />
+              <Route path="login" element={<Login />} />
+              <Route
+                path="app"
+                element={
+                  <ProtectedRoute>
+                    <AppLayout />
+                  </ProtectedRoute>
+                }
+              >
+                <Route index element={<Navigate to="cities" />} />
+                {/* Navigate serve como um redirecionamento de forma declarativa, ao contrário do useNavigate(). Nesse caso o usuário sempre irá cair no index que, por sua vez, o redicionará para cities. O replace serve para colocar o elemento atual na pilha de histórico */}
+                <Route path="cities" element={<CityList />} />
+                <Route path="cities/:id" element={<City />} />
+                <Route path="countries" element={<CountryList />} />
+                <Route path="form" element={<Form />} />
+              </Route>
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+          </Suspense>
         </BrowserRouter>
       </CitiesProvider>
     </AuthProvider>
